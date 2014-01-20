da5cdd43f978368489e208f5dda31767ed14984d
Intae Kim <greatkit@gmail.com>
2014-01-20 11:35:41 +0900

    9.1.1 Edit form

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -28,6 +28,7 @@ describe "AuthenticationPages" do
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before do
+        sign_in user  # Listing 9.5 suggests sole block, but can't???
         fill_in "Email",    with: user.email.upcase
         fill_in "Password", with: user.password
         click_button "Sign in"
@@ -35,6 +36,7 @@ describe "AuthenticationPages" do
 
       it { should have_title(user.name) }
       it { should have_link('Profile',     href: user_path(user)) }
+      it { should have_link('Settings',    href: edit_user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
 
