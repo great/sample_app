6e04e338c2adbebb0d73f82e5b246f5160a723bf
Intae Kim <greatkit@gmail.com>
2014-01-22 13:01:44 +0900

    11.3.1 Motivation and strategy
    (test is fail at this stage)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -125,7 +125,6 @@ describe User do
 
   # L 10.10
   describe "micropost associations" do
-
     before { @user.save }
     let! :older_micropost do
       FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
@@ -149,14 +148,26 @@ describe User do
     end
 
     # L 10.35
+    # L 11.41
     describe "status" do
       let(:unfollowed_post) do
         FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+      let(:followed_user) { FactoryGirl.create(:user) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
       end
 
       its(:feed) { should include(newer_micropost) }
       its(:feed) { should include(older_micropost) }
       its(:feed) { should_not include(unfollowed_post) }
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
     end
   end
 
